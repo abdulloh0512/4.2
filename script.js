84b89e9e6ff48c6d24aefb835227c1a77c7ac918
abdulloh0512 <abdullohdev0512@gmail.com>
2023-11-11 23:12:27 +0500

    4.2

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1-masala
+// let a = +"10";
+// let b = 5;
+// let c = a + b;
+// console.log(c);
+
+// 2-masala
+// let a;
+// a = +a;
+// console.log(a);
+
+// 3-masala
+// let a = 10;
+// console.log(typeof(a));
+
+// 4-masala
+// let a = prompt("Hohlagan qiymatni bering.")
+// console.log(Boolean(a));
+
+// 5-masala
+// let a = 22;
+// console.log(++a);
+
+// 6-masala
+// let a = 21;
+// console.log(--a);
+
+// 7-masala
+// let a = 10;
+// a = 0;
+// console.log(a);
